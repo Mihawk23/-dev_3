fe9df8a886f36f913ad7d44aaeb46366a2542af6
shn23 <127425991+Mihawk23@users.noreply.github.com>
2023-03-21 23:13:20 +0300

    Add files via upload

---
+++ ödev3.py
@@ -0,0 +1,13 @@
+# HTML Nedir?
+# HTML, Hyper Text Markup Language kelimelerinin kısaltılmışı halidir. Bu nedenle HTML bir programlama dili değil, bir işaretleme dili olarak ele alınır. HTML komutları, metin, resim, ses, video gibi içeriklerin sayfadaki yerleşimini ve ziyaretçiye gösterilmesini sağlar. HTML sayfalarının dosya uzantısı .htm veya .html’dir. HTML dili 1980 yılında CERN laboratuvarlarında görevli olan Tim Berners-Lee tarafından geliştirilmiştir.
+ 
+# click() = Seçilen öğeye tıklamak için kullanılır.
+# clear() = Seçilen öğenin içeriğini temizlemek için kullanılır. 
+# submit = Bir formu göndermek için kullanılır. 
+# select_by_visible_text = Bir dropdown  öğesinde belirli bir metne tıklamak için kullanılır.
+# Scroll = Sayfayı aşağı veya yukarı kaydırmak için kullanılır.
+# Switch to Frame = Bir iframe içine geçmek için kullanılır.
+# Switch to Window = Bir pencereye geçmek için kullanılır.
+# Back() = Önceki sayfaya gitmek için kullanılır.
+# Forward() = Sonraki sayfaya gitmek için kullanılır.
+# Refresh() = Sayfayı yenilemek için kullanılır.
